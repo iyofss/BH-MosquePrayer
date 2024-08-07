1f0525c65dc459e88cc468d1fe814795486853ea
iyofs <adyousif25@gmail.com>
2024-08-07 17:19:06 +0300

    Fixed variables names to new ui

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,38 +2,38 @@ extends Control
 
 
 # importing the time labels for the ui
-@onready var fajr_countdown = $MarginContainer/VBoxContainer/PrayerTimingContainer/FajrContainer/TimeContainer/FajrCountdown
-@onready var fajr_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/FajrContainer/TimeContainer/FajrTime
-@onready var fajr_iqama_offset = $MarginContainer/VBoxContainer/PrayerTimingContainer/FajrContainer/TimeContainer/IqamaContainer/FajrIqamaOffset
-@onready var fajr_iqama_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/FajrContainer/TimeContainer/IqamaContainer/FajrIqamaTime
+@onready var fajr_countdown = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/FajrContainer/TimeContainer/FajrCountdown
+@onready var fajr_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/FajrContainer/TimeContainer/FajrTime
+@onready var fajr_iqama_offset = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/FajrContainer/TimeContainer/IqamaContainer/FajrIqamaOffset
+@onready var fajr_iqama_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/FajrContainer/TimeContainer/IqamaContainer/FajrIqamaTime
 
-@onready var sunrise_countdown = $MarginContainer/VBoxContainer/PrayerTimingContainer/SunriseContainer/TimeContainer/SunriseCountdown
-@onready var sunrise_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/SunriseContainer/TimeContainer/SunriseTime
-@onready var sunrise_iqama_offset = $MarginContainer/VBoxContainer/PrayerTimingContainer/SunriseContainer/TimeContainer/IqamaContainer/SunriseIqamaOffset
-@onready var sunrise_iqama_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/SunriseContainer/TimeContainer/IqamaContainer/SunriseIqamaTime
+@onready var sunrise_countdown = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/SunriseContainer/TimeContainer/SunriseCountdown
+@onready var sunrise_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/SunriseContainer/TimeContainer/SunriseTime
+@onready var sunrise_iqama_offset = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/SunriseContainer/TimeContainer/IqamaContainer/SunriseIqamaOffset
+@onready var sunrise_iqama_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/SunriseContainer/TimeContainer/IqamaContainer/SunriseIqamaTime
 
-@onready var dhuhr_countdown = $MarginContainer/VBoxContainer/PrayerTimingContainer/DhuhrContainer/TimeContainer/DhuhrCountdown
-@onready var dhuhr_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/DhuhrContainer/TimeContainer/DhuhrTime
-@onready var dhuhr_iqama_offset = $MarginContainer/VBoxContainer/PrayerTimingContainer/DhuhrContainer/TimeContainer/IqamaContainer/DhuhrIqamaOffset
-@onready var dhuhr_iqama_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/DhuhrContainer/TimeContainer/IqamaContainer/DhuhrIqamaTime
+@onready var dhuhr_countdown = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/DhuhrContainer/TimeContainer/DhuhrCountdown
+@onready var dhuhr_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/DhuhrContainer/TimeContainer/DhuhrTime
+@onready var dhuhr_iqama_offset = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/DhuhrContainer/TimeContainer/IqamaContainer/DhuhrIqamaOffset
+@onready var dhuhr_iqama_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/DhuhrContainer/TimeContainer/IqamaContainer/DhuhrIqamaTime
 
-@onready var asr_countdown = $MarginContainer/VBoxContainer/PrayerTimingContainer/AsrContainer/TimeContainer/AsrCountdown
-@onready var asr_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/AsrContainer/TimeContainer/AsrTime
-@onready var asr_iqama_offset = $MarginContainer/VBoxContainer/PrayerTimingContainer/AsrContainer/TimeContainer/IqamaContainer/AsrIqamaOffset
-@onready var asr_iqama_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/AsrContainer/TimeContainer/IqamaContainer/AsrIqamaTime
+@onready var asr_countdown = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/AsrContainer/TimeContainer/AsrCountdown
+@onready var asr_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/AsrContainer/TimeContainer/AsrTime
+@onready var asr_iqama_offset = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/AsrContainer/TimeContainer/IqamaContainer/AsrIqamaOffset
+@onready var asr_iqama_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/AsrContainer/TimeContainer/IqamaContainer/AsrIqamaTime
 
-@onready var maghrib_countdown = $MarginContainer/VBoxContainer/PrayerTimingContainer/MaghribContainer/TimeContainer/MaghribCountdown
-@onready var maghrib_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/MaghribContainer/TimeContainer/MaghribTime
-@onready var maghrib_iqama_offset = $MarginContainer/VBoxContainer/PrayerTimingContainer/MaghribContainer/TimeContainer/IqamaContainer/MaghribIqamaOffset
-@onready var maghrib_iqama_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/MaghribContainer/TimeContainer/IqamaContainer/MaghribIqamaTime
+@onready var maghrib_countdown = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/MaghribContainer/TimeContainer/MaghribCountdown
+@onready var maghrib_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/MaghribContainer/TimeContainer/MaghribTime
+@onready var maghrib_iqama_offset = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/MaghribContainer/TimeContainer/IqamaContainer/MaghribIqamaOffset
+@onready var maghrib_iqama_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/MaghribContainer/TimeContainer/IqamaContainer/MaghribIqamaTime
 
-@onready var isha_countdown = $MarginContainer/VBoxContainer/PrayerTimingContainer/IshaContainer/TimeContainer/IshaCountdown
-@onready var isha_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/IshaContainer/TimeContainer/IshaTime
-@onready var isha_iqama_offset = $MarginContainer/VBoxContainer/PrayerTimingContainer/IshaContainer/TimeContainer/IqamaContainer/IshaIqamaOffset
-@onready var isha_iqama_time = $MarginContainer/VBoxContainer/PrayerTimingContainer/IshaContainer/TimeContainer/IqamaContainer/IshaIqamaTime
+@onready var isha_countdown = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/IshaContainer/TimeContainer/IshaCountdown
+@onready var isha_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/IshaContainer/TimeContainer/IshaTime
+@onready var isha_iqama_offset = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/IshaContainer/TimeContainer/IqamaContainer/IshaIqamaOffset
+@onready var isha_iqama_time = $VSplitContainer/VSplitContainer/VSplitContainer/MarginContainer/HSplitContainer/VBoxContainer/PrayerTimingContainer/IshaContainer/TimeContainer/IqamaContainer/IshaIqamaTime
 
 @onready var color_rect = $MarginContainer/ColorRect
-@onready var mosque_name_label = $MarginContainer/VBoxContainer/MosqueNameLabel
+@onready var mosque_name_label = $VSplitContainer/VSplitContainer/VSplitContainer/ColorRect2/VBoxContainer/MosqueNameLabel
 @onready var dhuhr_iqama_text_edit = $MarginContainer/ColorRect/VBoxContainer/HBoxContainer2/DhuhrIqamaTextEdit
 @onready var fajr_iqama_text_edit = $MarginContainer/ColorRect/VBoxContainer/HBoxContainer/FajrIqamaTextEdit
 @onready var asr_iqama_text_edit = $MarginContainer/ColorRect/VBoxContainer/HBoxContainer3/AsrIqamaTextEdit
